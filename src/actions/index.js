a5653722babf09b5aeda8f287ad05b5afe7ef71c
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-14 09:52:49 +0900

    add fetch user action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 import FETCH_USER from 'types';
 
-const fetchUser = () => {
-    axios.get('/api/current-user')
-};
+const fetchUser = () => dispatch => axios.get('/api/current-user')
+    .then(res => dispatch({ type : FETCH_USER, payload : res}));
