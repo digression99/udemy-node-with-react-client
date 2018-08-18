b83c89cdb81df692ed5456460bba4ff4f241d7f1
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-18 15:45:07 +0900

    refactor fetch user

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,12 @@
 
-export default (state = {}, action) => {
-    switch (action.type) {
+import { FETCH_USER } from '../actions/types';
 
+export default (state = {}, action) => {
+    console.log('action : ', action);
+
+    switch (action.type) {
+        case FETCH_USER:
+            return state;
         default:
             return state;
     }
